4b7c252168061b48e2345af997180938d9a4a57d
Eugene Kortov <kortovea@gmail.com>
2018-05-15 22:47:11 +0400

    Practiced Home Acrivity with DI and XML Config

---
+++ src/main/java/com/kortov/springworkshop/RandomFortuneService.java
@@ -0,0 +1,19 @@
+package com.kortov.springworkshop;
+
+import java.util.Random;
+
+public class RandomFortuneService implements FortuneService {
+
+    private String[] fortunes;
+
+    public RandomFortuneService() {
+        fortunes = new String[]{"Good luck!", "Way to go!", "Sheeeeit!"};
+    }
+
+    @Override
+    public String getFortune() {
+        Random random = new Random();
+        int index = random.nextInt(fortunes.length);
+        return fortunes[index];
+    }
+}
