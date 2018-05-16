9f91d48e1379692ddda23c9f714bf71c2ef752b2
Eugene Kortov <kortovea@gmail.com>
2018-05-16 21:40:15 +0400

    Practiced Activity for bean scopes and lifecycle with XML Config

--- src/main/java/com/kortov/springworkshop/BeanScopeDemoApp.java
+++ src/main/java/com/kortov/springworkshop/BeanScopeDemoApp.java
@@ -6,11 +6,21 @@ public class BeanScopeDemoApp {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
-        Coach theCoach = context.getBean("myCoach", Coach.class);
-        Coach alphaCoach = context.getBean("myCoach", Coach.class);
-        boolean result = (theCoach == alphaCoach);
-        System.out.println("\nPointing to the same object: " + result);
-        System.out.println("\nMemory location for theCoach: " + theCoach);
-        System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+        BaseballCoach baseballCoachNY = context.getBean("myBaseballCoach", BaseballCoach.class);
+        BaseballCoach baseballCoachLA = context.getBean("myBaseballCoach", BaseballCoach.class);
+        boolean baseballResult = (baseballCoachNY == baseballCoachLA);
+        System.out.println("\nBaseballCoaches (prototype) are pointing to the same object: " + baseballResult);
+        System.out.println("\nMemory location for baseballCoachNY: " + baseballCoachNY);
+        System.out.println("\nMemory location for baseballCoachLA: " + baseballCoachLA);
+        System.out.println("----------------------");
+
+        FootballCoach footballCoachBarcelona = context.getBean("myFootballCoach", FootballCoach.class);
+        FootballCoach footballCoachMadrid = context.getBean("myFootballCoach", FootballCoach.class);
+        boolean footballResult = (footballCoachBarcelona == footballCoachMadrid);
+        System.out.println("\nFootballCoaches (singleton) are pointing to the same object: " + footballResult);
+        System.out.println("\nMemory location for footballCoachBarcelona: " + footballCoachBarcelona);
+        System.out.println("\nMemory location for footballCoachMadrid: " + footballCoachMadrid);
+
+        context.close();
     }
 }
