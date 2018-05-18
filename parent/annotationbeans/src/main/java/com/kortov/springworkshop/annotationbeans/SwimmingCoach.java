50d8f83f8f17d78c67958712efb7ce60a076292e
Eugene Kortov <kortovea@gmail.com>
2018-05-18 22:39:29 +0400

    Practiced activity with beans IO Annotations

---
+++ parent/annotationbeans/src/main/java/com/kortov/springworkshop/annotationbeans/SwimmingCoach.java
@@ -0,0 +1,12 @@
+package com.kortov.springworkshop.annotationbeans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SwimmingCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Swim 1 kilometer";
+
+    }
+}
