016d13fb2b683b551497039269e7286ea061c9a8
kortovea <kortovea@gmail.com>
2018-05-25 21:33:31 +0400

    Practiced Annotations DI with fields when 4 beans are available

--- parent/annotationbeans/src/main/java/com/kortov/springworkshop/annotationbeans/TennisCoach.java
+++ parent/annotationbeans/src/main/java/com/kortov/springworkshop/annotationbeans/TennisCoach.java
@@ -1,20 +1,17 @@
 package com.kortov.springworkshop.annotationbeans;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+    @Autowired
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
     public TennisCoach() {
         System.out.println(">> TennisCoach: inside default constructor");
-    }
-
-    @Autowired
-    public void doSomeCrazyStuff(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside doSomeCrazyStuff method");
-        this.fortuneService = fortuneService;
     }
 
     @Override
