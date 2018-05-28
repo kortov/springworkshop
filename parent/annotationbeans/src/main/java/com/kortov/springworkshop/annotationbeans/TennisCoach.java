444eb287453931fd9e293197a397edd352941b2e
kortovea <kortovea@gmail.com>
2018-05-28 23:37:31 +0400

    Practice Activity Dependency Injection with Annotations, Bean Scopes with Annotations

--- parent/annotationbeans/src/main/java/com/kortov/springworkshop/annotationbeans/TennisCoach.java
+++ parent/annotationbeans/src/main/java/com/kortov/springworkshop/annotationbeans/TennisCoach.java
@@ -2,9 +2,14 @@ package com.kortov.springworkshop.annotationbeans;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope("singleton")
 public class TennisCoach implements Coach {
     @Autowired
     @Qualifier("fileFortuneService")
@@ -13,6 +18,17 @@ public class TennisCoach implements Coach {
     public TennisCoach() {
         System.out.println(">> TennisCoach: inside default constructor");
     }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
+    }
+
 
     @Override
     public String getDailyWorkout() {
