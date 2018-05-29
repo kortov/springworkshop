f6f5a8660b9b409cd75bb26c06374fce523ad65a
kortovea <kortovea@gmail.com>
2018-05-29 23:05:44 +0400

    Practice DI with Java Configuration with Bean annotation and values from properties file

--- parent/annotationbeans/src/main/java/com/kortov/springworkshop/annotationbeans/SportConfig.java
+++ parent/annotationbeans/src/main/java/com/kortov/springworkshop/annotationbeans/SportConfig.java
@@ -2,9 +2,11 @@ package com.kortov.springworkshop.annotationbeans;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan("com.kortov.springworkshop.annotationbeans")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
     @Bean
     public FortuneService sadFortuneService() {
