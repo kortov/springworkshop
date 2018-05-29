372278374d87a37c9e1cadaa2d224f0baaca60d3
kortovea <kortovea@gmail.com>
2018-05-29 22:29:38 +0400

    Practice DI with Java Configuration with Bean annotation

--- parent/annotationbeans/src/main/java/com/kortov/springworkshop/annotationbeans/SportConfig.java
+++ parent/annotationbeans/src/main/java/com/kortov/springworkshop/annotationbeans/SportConfig.java
@@ -1,9 +1,18 @@
 package com.kortov.springworkshop.annotationbeans;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.kortov.springworkshop.annotationbeans")
+//@ComponentScan("com.kortov.springworkshop.annotationbeans")
 public class SportConfig {
+    @Bean
+    public FortuneService sadFortuneService() {
+        return new SadFortuneService();
+    }
+
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach(sadFortuneService());
+    }
 }
---
+++ parent/annotationbeans/src/main/java/com/kortov/springworkshop/annotationbeans/SwimCoach.java
@@ -0,0 +1,19 @@
+package com.kortov.springworkshop.annotationbeans;
+
+public class SwimCoach implements Coach {
+    private FortuneService fortuneService;
+
+    public SwimCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Swim 1000 meters as a warmup";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+}
