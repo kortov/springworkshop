fa2d3bbce330041162bf7a812065180b402d3b37
kortovea <kortovea@gmail.com>
2018-05-29 22:15:27 +0400

    Practice DI with Java Configuration

---
+++ parent/annotationbeans/src/main/java/com/kortov/springworkshop/annotationbeans/SportConfig.java
@@ -0,0 +1,9 @@
+package com.kortov.springworkshop.annotationbeans;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.kortov.springworkshop.annotationbeans")
+public class SportConfig {
+}
