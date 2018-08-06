db9920eebf327d2b6eccc951cf21a8b336cfda92
kortovea <kortovea@gmail.com>
2018-08-06 13:49:14 +0400

    simple Hibernate read an object demo

---
+++ parent/hibernate/src/main/java/com/kortov/hibernate/ReadStudentDemo.java
@@ -0,0 +1,37 @@
+package com.kortov.hibernate;
+
+import com.kortov.hibernate.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ReadStudentDemo {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            System.out.println("Creating new student object...");
+            Student tempStudent = new Student("Daffy", "Duck", "daffy@luv2code.com");
+            session.beginTransaction();
+            System.out.println("Saving the student...");
+            System.out.println(tempStudent);
+            session.save(tempStudent);
+            session.getTransaction().commit();
+            System.out.println("Saved student. Generated id: " + tempStudent.getId());
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            System.out.println("\nGetting student with id: " + tempStudent.getId());
+            Student myStudent = session.get(Student.class, tempStudent.getId());
+            System.out.println("Get complete: " + myStudent);
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
