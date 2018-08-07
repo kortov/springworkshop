3274fa715ee1b43d39f8761967a701b9921d6dcc
kortovea <kortovea@gmail.com>
2018-08-07 13:17:29 +0400

    simple Hibernate one to one relationship

---
+++ parent/hibernate/hibernate1to1/src/main/java/com/kortov/hibernate/CreateDemo.java
@@ -0,0 +1,32 @@
+package com.kortov.hibernate;
+
+import com.kortov.hibernate.entity.Instructor;
+import com.kortov.hibernate.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateDemo {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            Instructor instructor = new Instructor("Jimmy", "McNulty", "wtfdidido@luv2code.com");
+            InstructorDetail instructorDetail = new InstructorDetail("https://www.youtube.com/user/mcnulty", "wtfdidido");
+            instructor.setInstructorDetail(instructorDetail);
+            session.beginTransaction();
+            System.out.println("Saving instructor: " + instructor);
+            session.save(instructor);
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
