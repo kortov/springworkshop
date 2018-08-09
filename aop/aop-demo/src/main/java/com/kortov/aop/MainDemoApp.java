c00fe69b77e291c989445b088b61252ab1480db9
kortovea <kortovea@gmail.com>
2018-08-09 21:11:30 +0400

    Simple AOP example @Before, wildcard for method parameter

--- aop/aop-demo/src/main/java/com/kortov/aop/MainDemoApp.java
+++ aop/aop-demo/src/main/java/com/kortov/aop/MainDemoApp.java
@@ -1,5 +1,6 @@
 package com.kortov.aop;
 
+import com.kortov.Account;
 import com.kortov.aop.dao.AccountDAO;
 import com.kortov.aop.dao.MembershipDAO;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,7 +10,7 @@ public class MainDemoApp {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
         AccountDAO accountDAO = context.getBean(AccountDAO.class);
         MembershipDAO membershipDAO = context.getBean(MembershipDAO.class);
-        accountDAO.addAccount();
+        accountDAO.addAccount(new Account());
         membershipDAO.addSillyStuff();
         context.close();
     }
--- aop/aop-demo/src/main/java/com/kortov/aop/dao/AccountDAO.java
+++ aop/aop-demo/src/main/java/com/kortov/aop/dao/AccountDAO.java
@@ -1,10 +1,11 @@
 package com.kortov.aop.dao;
 
+import com.kortov.Account;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AccountDAO {
-    public void addAccount() {
+    public void addAccount(Account account) {
         System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
     }
 }
