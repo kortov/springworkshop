d7b15e8b2e1d9e34e5485a3664383e4caa576d68
kortovea <kortovea@gmail.com>
2018-08-09 14:19:11 +0400

    Projects renaming, simple mvc-hibernate set up

---
+++ parent/mvc/web-customer-tracker/src/main/java/com/kortov/webcustomer/entity/Customer.java
@@ -0,0 +1,73 @@
+package com.kortov.webcustomer.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    @NotBlank(message = "is required")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotBlank(message = "is required")
+    private String lastName;
+
+    @Column(name = "email")
+    @Email
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
