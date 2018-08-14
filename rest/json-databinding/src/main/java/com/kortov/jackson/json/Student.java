97c376737ade52cd5a6e19709b864301731e0cb4
kortov <kortovea@gmail.com>
2018-08-14 10:43:16 +0400

    json databinding @JsonIgnoreProperties

--- rest/json-databinding/src/main/java/com/kortov/jackson/json/Student.java
+++ rest/json-databinding/src/main/java/com/kortov/jackson/json/Student.java
@@ -1,5 +1,8 @@
 package com.kortov.jackson.json;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
     private int id;
     private String firstName;
