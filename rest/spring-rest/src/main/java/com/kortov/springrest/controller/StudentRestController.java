814665a078db9ba53b53ffb30fae0e4db4b5d0c2
kortov <kortovea@gmail.com>
2018-08-14 12:02:14 +0400

    Rest Service @RestController get List

---
+++ rest/spring-rest/src/main/java/com/kortov/springrest/controller/StudentRestController.java
@@ -0,0 +1,29 @@
+package com.kortov.springrest.controller;
+
+import com.kortov.springrest.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> students() {
+        return students;
+    }
+}
