1d6c5d25f689c26d9afa33a4307a9652b066db65
kortov <kortovea@gmail.com>
2018-08-19 09:18:49 +0400

    mvc rest app insert, update, delete

--- rest/crm-rest/src/main/java/com/kortov/springdemo/rest/CustomerRestController.java
+++ rest/crm-rest/src/main/java/com/kortov/springdemo/rest/CustomerRestController.java
@@ -3,10 +3,7 @@ package com.kortov.springdemo.rest;
 import com.kortov.springdemo.entity.Customer;
 import com.kortov.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
@@ -29,5 +26,26 @@ public class CustomerRestController {
         Customer customer = Optional.ofNullable(customerService.getCustomer(customerId))
                 .orElseThrow(() -> new CustomerNotFoundException("Customer id not found -" + customerId));
         return customer;
+    }
+
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer) {
+        customer.setId(0);
+        customerService.saveCustomer(customer);
+        return customer;
+    }
+
+    @PutMapping("/customers")
+    public Customer udpdateCustomer(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
+        return customer;
+    }
+
+    @DeleteMapping("/customers/{customerId}")
+    public String deleteCustomer(@PathVariable int customerId) {
+        Optional.ofNullable(customerService.getCustomer(customerId))
+                .orElseThrow(() -> new CustomerNotFoundException("Customer id not found -" + customerId));
+        customerService.deleteCustomer(customerId);
+        return "Deleted customer id  - " + customerId;
     }
 }
